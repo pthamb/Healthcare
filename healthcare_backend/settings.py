6cce480b611073c04b93fb50ff57b832dd6039df
Purnendu Thamb <pthamb2002@gmail.com>
2025-04-24 22:23:30 +0530

    Updated the allowed host

--- healthcare_backend/settings.py
+++ healthcare_backend/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['healthcare-za4h.onrender.com']
 
 
 # Application definition
